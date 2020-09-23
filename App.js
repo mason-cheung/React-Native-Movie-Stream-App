96ad193ad237d3f01d883b51bcc1616bb925fa35
Mason <mason.chunho@gmail.com>
2020-09-24 04:15:56 +0800

    Added bottom tab bar with some style

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+//import liraries
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
-export default function App() {
+//import Screens
+import Home from './screens/Home'
+import Profile from './screens/Profile'
+import Recent from './screens/Recent'
+
+const Tab = createMaterialBottomTabNavigator();
+
+// create a component
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName='Home'
+        activeColor='#02ad94'
+        inactiveColor='#dedede'
+        style={{ backgroundColor: '#000' }}
+        barStyle={{ backgroundColor: '#0f0f0f', padding: 4 }}
+      >
+        <Tab.Screen 
+        name="Home" 
+        component={Home} 
+        options={{
+          tabBarLabel: '', tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name='home' color={color} size={28} />
+          )
+        }} />
+        <Tab.Screen name="Recent" component={Recent} />
+        <Tab.Screen name="Profile" component={Profile} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
 }
 
+// define your styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
+
+//make this component available to the app
+export default App;
